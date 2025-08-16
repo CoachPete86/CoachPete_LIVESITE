c3243b94a9e78ab2c84fdf7817022e76a63be8f5
CoachPete86 <coachpete86@icloud.com>
2025-08-16 20:45:37 +0100

    Create neonThemes.js

---
+++ src/src/assets/neonThemes.js
@@ -0,0 +1,14 @@
+export const neonThemes = {
+  'electric-blue': {
+    category: '#00ffff',
+    child: '#00bfff'
+  },
+  lime: {
+    category: '#ccff00',
+    child: '#99ff33'
+  },
+  magenta: {
+    category: '#ff00ff',
+    child: '#ff66cc'
+  }
+};
